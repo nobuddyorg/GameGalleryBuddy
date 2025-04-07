336dfa41a9fe452acfab017100b268c0fd4e0d74
Matthias Eggert <matthias.eggert@qytera.de>
2025-04-07 13:51:48 +0200

    add simple stupid test

---
+++ src/test/groovy/me/games/collection/CollectionApplicationTest.groovy
@@ -0,0 +1,13 @@
+package me.games.collection
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test will simply verify if the application context loads successfully
+    }
+}
