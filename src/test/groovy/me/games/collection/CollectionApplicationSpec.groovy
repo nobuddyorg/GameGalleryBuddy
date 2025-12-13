9fa835ad6bfefdc82b793a5614dc4b2dbeed49cd
Matthias Eggert <dem.matze@gmail.com>
2025-12-13 13:38:26 +0100

    fix update issues

--- src/test/groovy/me/games/collection/CollectionApplicationSpec.groovy
+++ src/test/groovy/me/games/collection/CollectionApplicationSpec.groovy
@@ -1,43 +1,41 @@
 package me.games.collection
 
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.boot.resttestclient.TestRestTemplate
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate
 import org.springframework.http.HttpStatus
-import org.springframework.web.context.WebApplicationContext
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestRestTemplate
+@AutoConfigureMockMvc
 class CollectionApplicationSpec {
 
-    @Autowired
-    WebApplicationContext context
+    @LocalServerPort
+    int port
 
     @Autowired
     TestRestTemplate restTemplate
 
+    @Autowired
     MockMvc mockMvc
 
     @Test
-    void contextLoads() {
-        assert context != null
-    }
-
-    @Test
-    void testControllerLoaded() throws Exception {
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).build()
+    void testControllerLoaded() {
         mockMvc.perform(get("/collection?username=john"))
                 .andExpect(status().isOk())
     }
 
     @Test
     void testFetchCollectionWithRestTemplate() {
-        def response = restTemplate.getForEntity("/collection?username=john", String)
+        def response = restTemplate.getForEntity("http://localhost:${port}/collection?username=john", String)
         assert response.statusCode == HttpStatus.OK
     }
 
